5134df47d560344d3b33662e23b6b8218e7f9d0b
Lorenzo Fernandez <lorenzojofernandez@gmail.com>
2020-11-15 11:51:23 +1300

    added page content

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,39 @@
 import React from "react"
+import Image from "../images/classic-tee.jpg"
+import "../styles/mystyles.css"
 
-export default function Home() {
-  return <div>Hello world!</div>
+const cartStyle = {
+  backgroundColor: '#F6F6F7',
+  width: '100%',
+  textAlign: 'right',
+  marginTop: '20px',
+  marginBottom: '20px',
+  color: '#888888',
 }
+
+const productPage = () => {
+return (
+  <div>
+    <div style={cartStyle}>My Cart </div>
+    <div class="flex-container"> 
+      <div>
+        <img class="flex-image" src={Image} alt="classic-tee" />
+      </div>
+      <div class="flex-description">
+        <h1>Classic Tee</h1>
+        <hr />
+        <p class="price">$75.00</p>
+        <hr />
+        <p>Dolor sit amet, consectetur adipiscing elit. Haec et tu ita posuisti, et verba vestra sunt. Quod autem ratione actum est, id officium appellamus dolor sit amet, consectetur adipiscing elit. Haec et tu ita posuisti, et verba vestra sunt. Quod autem ratione actum est, id officium appellamus</p>
+        <p>SIZE<span>*</span></p>
+        <button onclick="">S</button>
+        <button onclick="">M</button>
+        <button onclick="">L</button>
+        <button onclick="">Add to Cart</button>
+      </div>
+    </div>
+  </div>
+)}
+
+export default productPage
+
